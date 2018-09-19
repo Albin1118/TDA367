f4a366e0fc2734630137d6de39436b536890dcff
Adam Andersson <niklas.adam.andersson@gmail.com>
2018-09-19 19:48:33 +0200

    Created Exercise class

---
+++ app/src/main/java/com/example/ziggy/trainingtracker/Exercise.java
@@ -0,0 +1,12 @@
+package com.example.ziggy.trainingtracker;
+
+public class Exercise {
+    private String name;
+    private String description;
+
+
+    public Exercise(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
