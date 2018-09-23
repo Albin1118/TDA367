c8828ec018199a04d749012327d8327f8dc8a28b
Adam <niklas.adam.andersson@gmail.com>
2018-09-23 17:32:10 +0200

    Added a test for verifying the addExercise method

--- app/src/test/java/com/example/ziggy/trainingtracker/ExampleUnitTest.java
+++ app/src/test/java/com/example/ziggy/trainingtracker/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.ziggy.trainingtracker;
 
+import android.widget.EditText;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +14,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void addExercise_isCorrect() {
+        CreateExerciseActivity c = new CreateExerciseActivity();
+        String nameTest = "Name";
+        String descriptionTest = "Description";
+        c.createExercise(nameTest, descriptionTest);
+        assertEquals(c.customExercises.get(0).getName(), nameTest);
     }
 }
