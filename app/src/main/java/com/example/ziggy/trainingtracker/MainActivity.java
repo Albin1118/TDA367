9f8392931ac3c5719dd92b6685d9a3c57d1e164f
Adam Andersson <niklas.adam.andersson@gmail.com>
2018-09-24 19:50:56 +0200

    Removed classes and code according to new decisions made during group discussion

--- app/src/main/java/com/example/ziggy/trainingtracker/MainActivity.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/MainActivity.java
@@ -16,16 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.rick);
 
-
-        Button cardioButton = (Button) findViewById(R.id.cardioButton);
         Button strengthButton = (Button) findViewById(R.id.strengthButton);
 
-        cardioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CardioActivity.class));
-            }
-        });
+
 
         strengthButton.setOnClickListener(new View.OnClickListener() {
             @Override
