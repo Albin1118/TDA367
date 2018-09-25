c7223546f18150c84c9f194029f08204dba7afb6
Anders Magnusson <anders14magnusson@gmail.com>
2018-09-25 18:55:13 +0200

    Add TrainingTracker Class

---
+++ app/src/main/java/com/example/ziggy/trainingtracker/TrainingTracker.java
@@ -0,0 +1,11 @@
+package com.example.ziggy.trainingtracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainingTracker {
+    User user = new User();
+    List<Workout> workouts = new ArrayList<>();
+    List<Exercise> exercises = new ArrayList<>();
+
+}
