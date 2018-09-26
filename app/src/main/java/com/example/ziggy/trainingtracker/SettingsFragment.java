9aace43b9e6a688eb1a7f59dd4e6f2150fd2cc5b
Ziggy <frmax@student.chalmers.se>
2018-09-26 13:09:48 +0200

    Created SettingsFragment

---
+++ app/src/main/java/com/example/ziggy/trainingtracker/SettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.ziggy.trainingtracker;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SettingsFragment extends Fragment {
+
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        view  = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+        return view;
+    }
+}
