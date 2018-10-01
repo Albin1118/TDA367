eaaad0c53e75c11f144f35c6709617db8a6f2c5c
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-01 21:02:40 +0200

    Make MainActivity much more readable

--- app/src/main/java/com/example/ziggy/trainingtracker/view/MainActivity.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/view/MainActivity.java
@@ -15,95 +15,97 @@ import com.example.ziggy.trainingtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private BottomNavigationView bottomNavigationView;
     private ViewPager mViewPager;
-    MainViewModel viewModel;
-    BottomNavigationView bottomNavigationView;
-    MenuItem prevMenuItem;
+    private MenuItem prevMenuItem;
 
+    MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.viewPagerContainer);
-        mViewPager.addOnPageChangeListener(viewPageListener);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-
-        setupViewPager(mViewPager);
-
+        initViews();
+        initListeners();
         initDataBinding();
+        setupViewPager(mViewPager);
+    }
 
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        mViewPager = findViewById(R.id.viewPagerContainer);
+    }
+
+    private void initListeners() {
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment currentFragment = null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_dashboard:
+                        //currentFragment = new StartPageFragment();
+                        setViewPager(0);
+                        break;
+                    case R.id.nav_workouts:
+                        //currentFragment = new WorkoutTabFragment();
+                        setViewPager(1);
+                        break;
+                    case R.id.nav_exercises:
+                        //currentFragment = new ExercisesFragment();
+                        setViewPager(2);
+                        break;
+
+                    case R.id.nav_settings:
+                        //currentFragment = new StartPageFragment();
+                        setViewPager(3);
+                        break;
+
+
+                }
+
+                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                //       currentFragment).commit();
+                return true;
+            }
+        });
+    }
+
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 
     private void initDataBinding() {
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
     }
-
-   private  ViewPager.OnPageChangeListener viewPageListener = new ViewPager.OnPageChangeListener() {
-       @Override
-       public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-       }
-
-       @Override
-       public void onPageSelected(int position) {
-           if (prevMenuItem != null) {
-               prevMenuItem.setChecked(false);
-           }
-           else
-           {
-               bottomNavigationView.getMenu().getItem(0).setChecked(false);
-           }
-
-           bottomNavigationView.getMenu().getItem(position).setChecked(true);
-           prevMenuItem = bottomNavigationView.getMenu().getItem(position);
-
-       }
-
-       @Override
-       public void onPageScrollStateChanged(int state) {
-
-       }
-   };
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment currentFragment = null;
-
-            switch (menuItem.getItemId()){
-                case R.id.nav_dashboard:
-                    //currentFragment = new StartPageFragment();
-                    setViewPager(0);
-                    break;
-                case R.id.nav_workouts:
-                    //currentFragment = new WorkoutTabFragment();
-                    setViewPager(1);
-                    break;
-                case R.id.nav_exercises:
-                    //currentFragment = new ExercisesFragment();
-                    setViewPager(2);
-                    break;
-
-                case R.id.nav_settings:
-                    //currentFragment = new StartPageFragment();
-                    setViewPager(3);
-                    break;
-
-
-            }
-
-            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-             //       currentFragment).commit();
-            return true;
-        }
-    };
 
     public void setupViewPager(ViewPager viewpager) {
         SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
@@ -114,9 +116,5 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new ExerciseCreatorFragment());
         adapter.addFragment(new WorkoutCreatorFragment());
         viewpager.setAdapter(adapter);
-    }
-
-    public void setViewPager(int fragmentNumber){
-        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
