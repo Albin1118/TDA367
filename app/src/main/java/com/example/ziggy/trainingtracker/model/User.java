6fea3e515ffde29e9fa5697f1248232d64866247
Ziggy <frmax@student.chalmers.se>
2018-10-02 17:19:56 +0200

    Added functionality to User class

--- app/src/main/java/com/example/ziggy/trainingtracker/model/User.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/model/User.java
@@ -9,8 +9,55 @@ public class User {
     private ActiveWorkout activeWorkout;
     private List<Workout> finishedWorkouts;
     private List<Exercise> customExercises = new ArrayList<Exercise>();
+    private List<Exercise> customWorkouts = new ArrayList<Exercise>();
+
+    private String username;
+
+    //Data related to human qualities of user
+    private String name;
+    private double weight;
+    private int height;
+    //TODO is age really relevant?
+    private int age;
+
+
+    public User(String username, String name, double weight, int height) {
+        this.username = username;
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public List<Workout> getFinishedWorkouts() {
+        return finishedWorkouts;
+    }
+
+    public List<Exercise> getCustomWorkouts() {
+        return customWorkouts;
+    }
 
     public List<Exercise> getCustomExercises() {
         return customExercises;
+    }
+
+    //Weight might change
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
