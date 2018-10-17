122c95221fc149f0077878c388ca1e4fb900ff0b
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-18 00:04:21 +0200

    Add view model for ExerciseTab

--- app/src/main/java/com/example/ziggy/trainingtracker/view/ExerciseTabFragment.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/view/ExerciseTabFragment.java
@@ -1,7 +1,5 @@
 package com.example.ziggy.trainingtracker.view;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -15,14 +13,10 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.ziggy.trainingtracker.R;
 import com.example.ziggy.trainingtracker.model.Exercise;
-import com.example.ziggy.trainingtracker.model.ExerciseCategory;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.ziggy.trainingtracker.viewmodel.ExerciseTabViewModel;
 
 /**
  * Fragment representing the exercise-tab, where the list of exercises is displayed
@@ -31,60 +25,58 @@ public class ExerciseTabFragment extends Fragment {
 
     private FloatingActionButton addExerciseButton;
     private ListView exerciseListView;
+    private Button searchCategoryButton;
+    private Spinner exerciseCategorySpinner;
+
     private ArrayAdapter<Exercise> adapter;
 
     private MainActivity parentActivity;
     private View view;
-    private Button searchCategoryButton;
-    private Spinner exerciseCategorySpinner;
-    private List<Exercise>exercises;
-    private List<Exercise>customExercises;
+    private ExerciseTabViewModel viewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        parentActivity = ((MainActivity)getActivity());
+        parentActivity.setNavBarState(R.id.nav_exercises);
+        viewModel = new ExerciseTabViewModel();
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         view  = inflater.inflate(R.layout.fragment_exercise_tab, container, false);
-        parentActivity = ((MainActivity)getActivity());
-        parentActivity.setNavBarState(R.id.nav_exercises);
-        exercises = new ArrayList<>(parentActivity.viewModel.getExercises());
-
         initViews();
         initListeners();
-
-
-        adapter = new ArrayAdapter<Exercise>(getContext(), R.layout.exercise_list_item, R.id.exerciseNameTextView, exercises) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView exerciseNameTextView = (TextView) view.findViewById(R.id.exerciseNameTextView);
-                TextView exerciseDescriptionTextView = (TextView) view.findViewById(R.id.exerciseDescriptionTextView);
-
-                exerciseNameTextView.setText(exercises.get(position).getName());
-                exerciseDescriptionTextView.setText(exercises.get(position).getDescription());
-                return view;
-            }
-        };
-        exerciseListView.setAdapter(adapter);
 
         return view;
     }
 
     private void initViews() {
-
         //Initiate visual components by id
         addExerciseButton = view.findViewById(R.id.addExerciseButton);
         exerciseListView = view.findViewById(R.id.exerciseList);
         exerciseCategorySpinner = view.findViewById(R.id.exerciseCategorySpinner);
         searchCategoryButton = view.findViewById(R.id.searchCategoryButton);
 
-       //Style and populate spinner
-        ArrayList<String> categories = parentActivity.viewModel.getCategoriesToString();
-        ArrayAdapter<String> spinnerAdapter;
-        spinnerAdapter = new ArrayAdapter<>(this.parentActivity, android.R.layout.simple_spinner_item, categories);
+        adapter = new ArrayAdapter<Exercise>(getContext(), R.layout.exercise_list_item, R.id.exerciseNameTextView, viewModel.getFreshExercises()) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView exerciseNameTextView = (TextView) view.findViewById(R.id.exerciseNameTextView);
+                TextView exerciseDescriptionTextView = (TextView) view.findViewById(R.id.exerciseDescriptionTextView);
 
+                exerciseNameTextView.setText(viewModel.getExercises().get(position).getName());
+                exerciseDescriptionTextView.setText(viewModel.getExercises().get(position).getDescription());
+                return view;
+            }
+        };
+        exerciseListView.setAdapter(adapter);
+
+       //Style and populate spinner
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, viewModel.getCategories());
         //Dropdown layout style
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         //Attaching adapter to spinner
         exerciseCategorySpinner.setAdapter(spinnerAdapter);
     }
@@ -100,31 +92,18 @@ public class ExerciseTabFragment extends Fragment {
         exerciseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getContext(), parentActivity.viewModel.getExercises().get(position).toString(), Toast.LENGTH_SHORT).show();
                 ExerciseDetailViewFragment fragment = new ExerciseDetailViewFragment();
-                fragment.setExercise(exercises.get(position));
+                fragment.setExercise(viewModel.getExercises().get(position));
                 parentActivity.setFragmentContainerContent(fragment);
             }
         });
+
         searchCategoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ExerciseCategory category = ExerciseCategory.valueOf(exerciseCategorySpinner.getSelectedItem().toString());
-                List<Exercise> exercisesInCategory = new ArrayList<>();
-                for(int i=0; i<parentActivity.viewModel.getExercises().size(); i++) {
-                    if (parentActivity.viewModel.getExercises().get(i).getCategories().contains(category)) {
-                        exercisesInCategory.add(parentActivity.viewModel.getExercises().get(i));
-                    }
-                }
-                setExercises(exercisesInCategory);
+                viewModel.sortExercisesByCategory(exerciseCategorySpinner.getSelectedItem().toString());
                 adapter.notifyDataSetChanged();
-
             }
         });
-    }
-
-    public void setExercises(List<Exercise> exercises) {
-        this.exercises.clear();
-        this.exercises.addAll(exercises);
     }
 }
---
+++ app/src/main/java/com/example/ziggy/trainingtracker/viewmodel/ExerciseTabViewModel.java
@@ -0,0 +1,46 @@
+package com.example.ziggy.trainingtracker.viewmodel;
+
+import com.example.ziggy.trainingtracker.model.Exercise;
+import com.example.ziggy.trainingtracker.model.ExerciseCategory;
+import com.example.ziggy.trainingtracker.model.TrainingTracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseTabViewModel {
+    TrainingTracker model = TrainingTracker.getInstance();
+    private List<Exercise> exercises = new ArrayList<>(model.getExercises());
+
+    public ExerciseTabViewModel() {
+    }
+
+    public void sortExercisesByCategory(String categoryString) {
+        ExerciseCategory category = ExerciseCategory.valueOf(categoryString);
+
+        List<Exercise> sortedExercises = new ArrayList<>();
+        for (Exercise e : model.getExercises()) {
+            if (e.getCategories().contains(category))
+                sortedExercises.add(e);
+        }
+        exercises.clear();
+        exercises.addAll(sortedExercises);
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public List<Exercise> getFreshExercises() {
+        exercises.clear();
+        exercises.addAll(model.getExercises());
+        return exercises;
+    }
+
+    public List<String> getCategories() {
+        List<String> categories = new ArrayList<>();
+        for (ExerciseCategory category : ExerciseCategory.values()) {
+            categories.add(category.name());
+        }
+        return categories;
+    }
+}
