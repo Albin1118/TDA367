f39563d6406f1d9c9f07f5485473648b60dd8b8a
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-20 16:14:59 +0200

    Implement night mode switch -does not work flawlessly, the activity gets recreated and you are sent back to the home screen with the more tab still selected in the bottom nav bar

--- app/src/main/java/com/example/ziggy/trainingtracker/view/MainActivity.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/view/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements NavigationManager
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(getSharedPreferences("Themes", 0).getInt("theme", 0));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
--- app/src/main/java/com/example/ziggy/trainingtracker/view/MoreTabFragment.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/view/MoreTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.ziggy.trainingtracker.view;
 
+import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import com.example.ziggy.trainingtracker.R;
 
@@ -18,12 +21,20 @@ import com.example.ziggy.trainingtracker.R;
 public class MoreTabFragment extends Fragment {
 
 
-    private View view;
     private EditText weightEditText;
+    private Switch nightModeSwitch;
     private Button statisticsButton;
 
+    private View view;
     private NavigationManager navigationManager;
 
+    SharedPreferences themeSettings;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        themeSettings = getActivity().getSharedPreferences("Themes", 0);
+    }
 
     @Nullable
     @Override
@@ -40,7 +51,12 @@ public class MoreTabFragment extends Fragment {
 
     private void initViews() {
         weightEditText = view.findViewById(R.id.weight_edit_text);
+        nightModeSwitch = view.findViewById(R.id.night_mode_switch);
         statisticsButton = view.findViewById(R.id.statistics_button);
+
+        if (themeSettings.getInt("theme", 0) == R.style.DarkTheme) {
+            nightModeSwitch.setChecked(true);
+        }
     }
 
 
@@ -49,6 +65,20 @@ public class MoreTabFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 navigationManager.navigateStatistics();
+            }
+        });
+
+        nightModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor themeSettingsEditor = themeSettings.edit();
+                if (isChecked) {
+                    themeSettingsEditor.putInt("theme", R.style.DarkTheme);
+                } else {
+                    themeSettingsEditor.putInt("theme", R.style.AppTheme);
+                }
+                themeSettingsEditor.apply();
+                getActivity().recreate();
             }
         });
     }
