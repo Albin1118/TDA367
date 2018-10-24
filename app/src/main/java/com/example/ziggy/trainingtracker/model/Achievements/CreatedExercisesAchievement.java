aaccff1e3f2614979c02b84d7c830d6b516b5b4b
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-25 01:44:28 +0200

    Create a new achievement type FinishedWorkoutsAchievement

--- app/src/main/java/com/example/ziggy/trainingtracker/model/Achievements/CreatedExercisesAchievement.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/model/Achievements/CreatedExercisesAchievement.java
@@ -2,19 +2,19 @@ package com.example.ziggy.trainingtracker.model.Achievements;
 
 import com.example.ziggy.trainingtracker.model.IUser;
 
-public class CreateExercisesAchievement extends Achievement {
-    private static final String INFO = "Create your own exercises by clicking the \"+\" symbol in the exercises tab";
+public class CreatedExercisesAchievement extends Achievement {
+    private static final String INFO = "Create your own exercises by clicking the \"+\" symbol in the exercises tab.";
     private static final int START = 1;
     private static final int MAX = 10;
 
     private int stage;
 
-    CreateExercisesAchievement() {
+    public CreatedExercisesAchievement() {
         super(START, "Create " + START + " Exercise" + (START > 1 ? "s" : ""), INFO);
         stage = 1;
     }
 
-    private CreateExercisesAchievement(int requirement, int stage) {
+    private CreatedExercisesAchievement(int requirement, int stage) {
         super(requirement, "Create " + requirement + " Exercises", INFO);
         this.stage = stage;
     }
@@ -31,17 +31,15 @@ public class CreateExercisesAchievement extends Achievement {
 
     @Override
     protected Achievement getNextAchievement() {
-        return new CreateExercisesAchievement(nextRequirement(), stage + 1);
+        return new CreatedExercisesAchievement(nextRequirement(), stage + 1);
     }
 
     private int nextRequirement() {
-        switch (stage + 1) {
-            case 2:
+        switch (stage) {
+            case 1:
                 return 3;
-            case 3:
-                return 5;
             default:
-                return stage*5 - 5;
+                return (stage - 1) * 5;
         }
     }
 }
---
+++ app/src/main/java/com/example/ziggy/trainingtracker/model/Achievements/FinishedWorkoutsAchievement.java
@@ -0,0 +1,53 @@
+package com.example.ziggy.trainingtracker.model.Achievements;
+
+import com.example.ziggy.trainingtracker.model.IUser;
+
+public class FinishedWorkoutsAchievement extends Achievement {
+    private static final String INFO = "Go to the workout tab, select a workout and click on start workout.";
+    private static final int START = 1;
+    private static final int MAX = 10;
+
+    private int stage;
+
+    public FinishedWorkoutsAchievement() {
+        super(START, "Finish " + START + " Workout" + (START > 1 ? "s" : ""), INFO);
+        stage = 1;
+    }
+
+    private FinishedWorkoutsAchievement(int requirement, int stage) {
+        super(requirement, "Finish " + requirement + " Workouts", INFO);
+        this.stage = stage;
+    }
+
+    @Override
+    protected int checkProgress(IUser user) {
+        return user.getFinishedWorkouts().size();
+    }
+
+    @Override
+    protected boolean hasNextStage() {
+        return true;
+    }
+
+    @Override
+    protected Achievement getNextAchievement() {
+        return new FinishedWorkoutsAchievement(nextRequirement(), stage + 1);
+    }
+
+    private int nextRequirement() {
+        switch (stage) {
+            case 1:
+                return 5;
+            case 2:
+                return 10;
+            case 3:
+                return 25;
+            case 4:
+                return 50;
+            case 5:
+                return 75;
+            default:
+                return (stage - 4) * 50;
+        }
+    }
+}
