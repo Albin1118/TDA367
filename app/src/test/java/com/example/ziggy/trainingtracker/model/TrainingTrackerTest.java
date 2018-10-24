cf5ae545a38899f4b4838a4ca7ab932cb77a7f75
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-24 17:59:58 +0200

    Add a removeCustomWorkout test

--- app/src/test/java/com/example/ziggy/trainingtracker/model/TrainingTrackerTest.java
+++ app/src/test/java/com/example/ziggy/trainingtracker/model/TrainingTrackerTest.java
@@ -8,11 +8,12 @@ public class TrainingTrackerTest {
 
     @Test
     public void addCustomExercise_isCorrect() {
-        TrainingTracker tracker = new TrainingTracker();
+        ITrainingTracker tracker = new TrainingTracker();
         String name = "Pushups", description = "Very hard", instructions = "Don´t hit your nose", unit = "reps";
         List<ExerciseCategory> categories = new ArrayList<>();
         categories.add(ExerciseCategory.ARMS);
         tracker.addCustomExercise(new Exercise(name, unit, description, instructions, categories));
+        assertEquals(tracker.getUser().getCustomExercises().size(), 1);
         assertEquals(tracker.getUser().getCustomExercises().get(0).getName(), name);
         assertEquals(tracker.getUser().getCustomExercises().get(0).getUnit(), unit);
         assertEquals(tracker.getUser().getCustomExercises().get(0).getDescription(), description);
@@ -24,28 +25,42 @@ public class TrainingTrackerTest {
 
     @Test
     public void removeCustomExercise_isCorrect() {
-        TrainingTracker tracker = new TrainingTracker();
+        ITrainingTracker tracker = new TrainingTracker();
         String name = "Pushups", description = "Very hard", instructions = "Don´t hit your nose", unit = "reps";
         List<ExerciseCategory> categories = new ArrayList<>();
         categories.add(ExerciseCategory.ARMS);
         Exercise e = new Exercise(name, unit, description, instructions, categories);
-        assertEquals(tracker.getUser().getCustomExercises().size(), 0);
         tracker.addCustomExercise(e);
         assertEquals(tracker.getUser().getCustomExercises().size(), 1);
         tracker.removeCustomExercise(e);
         assertEquals(tracker.getUser().getCustomExercises().size(), 0);
     }
+
     @Test
     public void addCustomWorkout_isCorrect() {
-        TrainingTracker tracker = new TrainingTracker();
+        ITrainingTracker tracker = new TrainingTracker();
         String name = "Hard workout", description = "Very hard";
         List<IWorkoutBlock> blocks = new ArrayList<>();
         blocks.add(new WorkoutBlock());
         IWorkout w = new Workout(name, description, blocks);
         tracker.addCustomWorkout(w);
+        assertEquals(tracker.getUser().getCustomWorkouts().size(), 1);
         assertEquals(tracker.getUser().getCustomWorkouts().get(0).getName(), name);
         assertEquals(tracker.getUser().getCustomWorkouts().get(0).getDescription(), description);
         assertEquals(tracker.getUser().getCustomWorkouts().get(0).getBlocks(), blocks);
+    }
+
+    @Test
+    public void removeCustomWorkout_isCorrect() {
+        ITrainingTracker tracker = new TrainingTracker();
+        String name = "Hard workout", description = "Very hard";
+        List<IWorkoutBlock> blocks = new ArrayList<>();
+        blocks.add(new WorkoutBlock());
+        IWorkout w = new Workout(name, description, blocks);
+        tracker.addCustomWorkout(w);
+        assertEquals(tracker.getUser().getCustomWorkouts().size(), 1);
+        tracker.removeCustomWorkout(w);
+        assertEquals(tracker.getUser().getCustomWorkouts().size(), 0);
     }
 
 }
