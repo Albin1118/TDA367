a090f4aea7f2ceed44940f91c4cec379d9d4b655
Anders Magnusson <anders14magnusson@gmail.com>
2018-10-27 17:51:19 +0200

    Add comments in Achievement

--- app/src/main/java/com/example/ziggy/trainingtracker/model/Achievement.java
+++ app/src/main/java/com/example/ziggy/trainingtracker/model/Achievement.java
@@ -18,6 +18,11 @@ public abstract class Achievement {
         this.info = info;
     }
 
+    /**
+     * Update the state of the achievement pulling information from the user.
+     * If completed, add the next achievement in line.
+     * @param user Owner of the achievement to get the information from
+     */
     void update(IUser user) {
         if (!completed) {
             progress = checkProgress(user);
@@ -33,28 +38,56 @@ public abstract class Achievement {
         }
     }
 
+    /**
+     * Get the updated progress number using information from the user.
+     * @param user Owner of the achievement to get the information from
+     * @return
+     */
     protected abstract int checkProgress(IUser user);
 
+    /**
+     * Check if the achievement has a successor.
+     * @return
+     */
     protected abstract boolean hasNextStage();
 
+    /**
+     * Get the succeeding achievement.
+     * @return
+     */
     protected abstract Achievement getNextAchievement();
 
+    /**
+     * @return the achievement's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the achievement's info
+     */
     public String getInfo() {
         return info;
     }
 
+    /**
+     * @return the achievement's requirement
+     */
     public int getRequirement() {
         return requirement;
     }
 
+    /**
+     * @return the achievement's progress
+     */
     public int getProgress() {
         return progress;
     }
 
+    /**
+     * @return the achievement's completion status
+     */
     public boolean isCompleted() {
         return completed;
     }
